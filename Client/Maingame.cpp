4e42cd4674c0eb16d814c9f874310fed57e55ef9
Catoblespa-NB <a4ka2@naver.com>
2019-09-09 11:54:43 +0900

    ㅠㅠ

--- Client/Maingame.cpp
+++ Client/Maingame.cpp
@@ -17,12 +17,15 @@ CMaingame::~CMaingame()
 	Release();
 }
 
+
 void CMaingame::Update()
 {
+
 	m_pTimeMgr->UpdateTime();
 	m_pKeyMgr->Update();
 	m_pTerrain->Update();
 }
+
 
 void CMaingame::LateUpdate()
 {
@@ -49,24 +52,33 @@ HRESULT CMaingame::Initialize()
 	hr = m_pDeviceMgr->InitDevice(CDeviceMgr::MODE_WIN);
 	FAILED_CHECK_MSG_RETURN(hr, L"장치초기화 실패", E_FAIL);
 
-	hr = m_pTextureMgr->LoadTexture(
-		CTextureMgr::SINGLE_TEXTURE, L"../Texture/Cube.png", L"Cube");
-	FAILED_CHECK_MSG_RETURN(hr, L"Cube Texture Load Failed", E_FAIL);
+	//hr = m_pTextureMgr->LoadTexture(
+	//	CTextureMgr::SINGLE_TEXTURE, L"../Texture/Cube.png", L"Cube");
+	//FAILED_CHECK_MSG_RETURN(hr, L"Cube Texture Load Failed", E_FAIL);
+
+	//hr = m_pTextureMgr->LoadTexture(
+	//	CTextureMgr::MULTI_TEXTURE, L"../Texture/Stage/Player/Attack/AKIHA_AKI01_00%d.png",
+	//	L"Player", L"Attack", 6);
+	//FAILED_CHECK_MSG_RETURN(hr, L"Player Attack Texture Load Failed", E_FAIL);
+
+	//hr = m_pTextureMgr->LoadTexture(
+	//	CTextureMgr::MULTI_TEXTURE, L"../Texture/Stage/Player/Dash/AKIHA_AKI13_00%d.png",
+	//	L"Player", L"Dash", 11);
+	//FAILED_CHECK_MSG_RETURN(hr, L"Player Dash Texture Load Failed", E_FAIL);
+
+	//hr = m_pTextureMgr->LoadTexture(
+	//	CTextureMgr::MULTI_TEXTURE, L"../Texture/Stage/Terrain/Tile/Tile%d.png",
+	//	L"Terrain", L"Tile", 38);
+	//FAILED_CHECK_MSG_RETURN(hr, L"Terrain Tile Texture Load Failed", E_FAIL);
+
+
 
 	hr = m_pTextureMgr->LoadTexture(
-		CTextureMgr::MULTI_TEXTURE, L"../Texture/Stage/Player/Attack/AKIHA_AKI01_00%d.png",
-		L"Player", L"Attack", 6);
-	FAILED_CHECK_MSG_RETURN(hr, L"Player Attack Texture Load Failed", E_FAIL);
+		CTextureMgr::MULTI_TEXTURE, L"../Texture/Stage/Terrain/Tile2/Tile__0%d.png",
+		L"Terrain", L"Tile2", 38);
+	FAILED_CHECK_MSG_RETURN(hr, L"Terrain Tile2 Texture Load Failed", E_FAIL);
 
-	hr = m_pTextureMgr->LoadTexture(
-		CTextureMgr::MULTI_TEXTURE, L"../Texture/Stage/Player/Dash/AKIHA_AKI13_00%d.png",
-		L"Player", L"Dash", 11);
-	FAILED_CHECK_MSG_RETURN(hr, L"Player Dash Texture Load Failed", E_FAIL);
 
-	hr = m_pTextureMgr->LoadTexture(
-		CTextureMgr::MULTI_TEXTURE, L"../Texture/Stage/Terrain/Tile/Tile%d.png",
-		L"Terrain", L"Tile", 38);
-	FAILED_CHECK_MSG_RETURN(hr, L"Terrain Tile Texture Load Failed", E_FAIL);
 
 	// Terrain
 	m_pTerrain = CTerrain::Create();
